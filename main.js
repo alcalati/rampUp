6293fb76bbc49c15c50de3a3bd4e234902931cb3
alcalati <alcaat@edem.es>
2024-04-13 13:13:25 +0200

    final2

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ function fibonacci(firstNumber, secondNumber, limit) {
   let aux = 0;  // Para guardar suma
 
   result.push(firstNumber); //Mete los 2 primeros números en el array
+  console.log(firstNumber);
   result.push(secondNumber);
+  console.log(secondNumber);
 
   aux = firstNumber + secondNumber; //Primera suma
-
+  console.log(aux);
   while (aux < limit) { // Bucle que suma hasta que se llegue al limite
     result.push(aux);
+    console.log(aux);
     firstNumber = secondNumber;
     secondNumber = aux;
     aux = firstNumber + secondNumber;
